f5acbb93d29b22f6557b49433e89d3edbac2b841
Ashuraliyev-Muhammadumar <ashiraliyevmuhammadumar@gmail.com>
2022-10-10 02:39:34 +0500

    misol

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function removeStringSpaces(){
+    let namesOf = ["hello", 45, -3, null, true, undefined, false,{age: 40} ] ;
+    let math = Math.floor(Math.random() * 15);
+    console.log(namesOf[math]);
+}
+removeStringSpaces()
